c560acf0eb59106f472384ce85f19af51d53bbb1
Burak-Alkan <alkanburak10@gmail.com>
2025-02-26 03:51:11 +0300

    context oluşturuldu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import useDarkMode from "../hooks/useDarkMode";
-
+import { useContext } from "react";
+import { ContextLanguage } from "../context/LanguageContexts";
 const Navbar = () => {
   const [isDarkMode,setIsDarkMode]=useDarkMode();
+
+  const {dilDegistir,content}=useContext(ContextLanguage)
+  
+  const changeLanguage=()=>
+  {
+    dilDegistir("tr")
+  }
   return (
     <div className="navbar">
       <div className="navbar-name">A</div>
       <div className="navbar-links">
-        <a href="#" className="navbar-link-item">Skills</a>
+        <a href="#" className="navbar-link-item">{content.skils}</a>
         <a href="#" className="navbar-link-item">Projects</a>
         <a href="#" className="navbar-link-item">Hire me</a>
 
         <button onClick={()=>setIsDarkMode(!isDarkMode)}>
           sss
+        </button>
+
+        <button onClick={changeLanguage}>
+          dili değistir
         </button>
         
       </div>
